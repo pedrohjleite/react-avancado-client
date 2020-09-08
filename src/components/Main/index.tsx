086900fe97354404875a18e5000912931943c093
Pedro Henrique Jansen Leite <pedrohjleite@Pedros-MacBook-Pro.local>
2020-09-08 14:44:15 -0300

    styled-components pattern

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,21 +1,21 @@
-import * as S from './styles';
+import { Wrapper, Logo, Title, Description, Illustration } from './styles';
 
 const Main = ({
   title = 'React Avançado',
   description = 'TypeScript, ReactJS, NextJS e Styled Components',
 }) => (
-  <S.Wrapper>
-    <S.Logo
+  <Wrapper>
+    <Logo
       src="/img/logo.svg"
       alt="Imagem de um átomo e React Avançado escrito ao lado."
     />
-    <S.Title>{title}</S.Title>
-    <S.Description>{description}</S.Description>
-    <S.Illustration
+    <Title>{title}</Title>
+    <Description>{description}</Description>
+    <Illustration
       src="/img/hero-illustration.svg"
       alt="Um desenvolvedor de frente para uma tela com código."
     />
-  </S.Wrapper>
+  </Wrapper>
 );
 
 export default Main;
